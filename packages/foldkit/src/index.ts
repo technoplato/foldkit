0cfc20da9ac6a3412d12cf7bd41af51623e3bd0d
Devin Jameson <devinjameson@hey.com>
2025-09-14 19:03:34 -0400

    align version in index with package.json

--- packages/foldkit/src/index.ts
+++ packages/foldkit/src/index.ts
@@ -1,4 +1,4 @@
-export const VERSION = '0.1.0-canary.2'
+export const VERSION = '0.1.0-canary.7'
 
 // TODO: Should this be called FormValidation or FieldValidation?
 export * as FieldValidation from './core/fieldValidation'
