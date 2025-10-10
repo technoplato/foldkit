471d2a77ab1e99f63446daaaad1641f7a720e4ab
Devin Jameson <devinjameson@hey.com>
2025-10-09 21:54:29 -0400

    chore(cfa): use latest foldkit version

--- packages/create-foldkit-app/src/index.ts
+++ packages/create-foldkit-app/src/index.ts
@@ -78,7 +78,7 @@ const create = Command.make(
 
 const cli = Command.run(create, {
   name: 'Create Foldkit App',
-  version: '0.1.5',
+  version: '0.1.6',
   summary: HelpDoc.getSpan(HelpDoc.p('Create a new Foldkit application')),
 })
 
